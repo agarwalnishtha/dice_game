068ffdba93d0fee0c9df7fb605f656132a1aeb10
agarwalnishtha <35678934+agarwalnishtha@users.noreply.github.com>
2021-01-27 09:15:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random() * 6)+1;
 var randomNumber2 = Math.floor(Math.random() * 6)+1;
 
-document.querySelector(".img1").setAttribute("src","images/dice"+ randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+ randomNumber2 + ".png");
+document.querySelector(".img1").setAttribute("src","dice"+ randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src","dice"+ randomNumber2 + ".png");
 
 var player1 = prompt("Enter name of player1");
 var player2 = prompt("Enter name of player2");
